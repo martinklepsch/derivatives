733fb6a561ac5f226cbc94b4671f7e826cdc5dbf
Martin Klepsch <martinklepsch@googlemail.com>
2016-12-04 17:12:11 +0700

    build out example app with multiple usage scenarios

--- example/drv_example/app.cljs
+++ example/drv_example/app.cljs
@@ -10,32 +10,87 @@
    :as-map [[:base :inc] (fn [base inc] {:base base :after-inc inc})]
    :sum    [[:as-map]    (fn [as-map] (+ (:base as-map) (:after-inc as-map)))]})
 
+(def container :pre.bg-black-05.pa3.ma0.br2)
+(def item :span.db.mb2)
+
 (rum/defcs derived-view < rum/reactive (d/drv :base) (d/drv :inc) (d/drv :as-map) (d/drv :sum)
   [s]
-  [:code
-   [:p ":base "   (-> (d/react s :base) pr-str)]
-   [:p ":inc "    (-> (d/react s :inc) pr-str)]
-   [:p ":as-map " (-> (d/react s :as-map) pr-str)]
-   [:p ":sum "    (-> (d/react s :sum) pr-str)]])
+  [:div
+   [item "Using multiple " [:code "drv"] " mixins"]
+   [container
+    [item ":base "   (-> (d/react s :base) pr-str)]
+    [item ":inc "    (-> (d/react s :inc) pr-str)]
+    [item ":as-map " (-> (d/react s :as-map) pr-str)]
+    [item ":sum "    (-> (d/react s :sum) pr-str)]]])
+
+(rum/defcs derived-view-variadic-mixin < rum/reactive (d/drv :base :inc :as-map :sum)
+  [s]
+  [:div
+   [item "Using single, variadic " [:code "drv"] " mixin"]
+   [container
+    [item ":base "   (-> (d/react s :base) pr-str)]
+    [item ":inc "    (-> (d/react s :inc) pr-str)]
+    [item ":as-map " (-> (d/react s :as-map) pr-str)]
+    [item ":sum "    (-> (d/react s :sum) pr-str)]]])
+
+(rum/defcs derived-view-react-drvs-all < rum/reactive (d/drv :inc :as-map :sum) (d/drv :base)
+  [s]
+  [:div
+   [item "Both mixin varians, " [:code "react-drvs"]]
+   (let [{:keys [base inc as-map sum]} (d/react-drvs s)]
+     [container
+      [item ":base "   (-> base pr-str)]
+      [item ":inc "    (-> inc pr-str)]
+      [item ":as-map " (-> as-map pr-str)]
+      [item ":sum "    (-> sum pr-str)]])])
+
+(rum/defcs derived-view-react-both < rum/reactive (d/drv :inc :as-map :sum) (d/drv :base)
+  [s]
+  [:div
+   [item "Both mixin varians, " [:code "react-drvs"] " and singular " [:code "react"]]
+   (let [{:keys [base inc]} (d/react-drvs s :base :inc)]
+     [container
+      [item ":base "   (-> base pr-str)]
+      [item ":inc "    (-> inc pr-str)]
+      [item ":as-map " (-> (d/react s :as-map) pr-str)]
+      [item ":sum "    (-> (d/react s :sum) pr-str)]])])
 
 (defonce *state (atom 0))
+
+(def button :button.f6.fw6.ttu.link.dim.br1.ba.ph3.pv2.mb2.mr2.dib.white.pointer)
+
+(rum/defc heading []
+  [:div.mb3.pa2
+   [:h1 "Derivatives Dataflow Test"]
+   [button {:on-click #(swap! *state inc)} "inc"]
+   [button {:on-click #(prn @*state)} "log state atom"]])
+
+(rum/defc drv-variant-table []
+  [:div.cf
+   [:div.fl.w-50.pa2 (derived-view)]
+   [:div.fl.w-50.pa2 (derived-view-variadic-mixin)]
+   [:div.fl.w-50.pa2 (derived-view-react-drvs-all)]
+   [:div.fl.w-50.pa2 (derived-view-react-both)]])
 
 (rum/defc dataflow-test < (d/rum-derivatives (drv-spec *state))
   []
   [:div
-   [:h1 "Dataflow Test"]
-   [:button {:on-click #(prn @*state)} "log *state"]
-   [:button {:on-click #(swap! *state inc)} "inc"]
-   (derived-view)])
+   [:h3.mb4.pa2 "Derviatives spec passed to mixin"]
+   (drv-variant-table)])
 
 (rum/defc dataflow-test* < (d/rum-derivatives* first)
   [spec]
   [:div
-   [:h1 "Dataflow Test"]
-   [:button {:on-click #(prn @*state)} "log *state"]
-   [:button {:on-click #(swap! *state inc)} "inc"]
-   (derived-view)])
+   [:h3.mb4.pa2 "Derviatives spec extracted from arguments"]
+   (drv-variant-table)])
+
+(rum/defc root-component []
+  [:div.pa4
+   (heading)
+   [:hr.mv3.ba]
+   (dataflow-test* (drv-spec *state))
+   [:hr.mv3.ba]
+   (dataflow-test)])
 
 (defn init []
-  (rum/mount (dataflow-test* (drv-spec *state))
-             (js/document.getElementById "container")))
+  (rum/mount (root-component) (js/document.getElementById "container")))
